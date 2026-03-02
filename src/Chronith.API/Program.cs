571fcfe94167c52629833175629a4e06dc49a94d
Julius Bartolome <juliusbartolome@duck.com>
2026-03-02 12:37:09 +0800

    fix(api): run ef core migrations on startup via MigrateAsync

--- src/Chronith.API/Program.cs
+++ src/Chronith.API/Program.cs
@@ -2,9 +2,11 @@ using Chronith.API.HealthChecks;
 using Chronith.API.Middleware;
 using Chronith.Application;
 using Chronith.Infrastructure;
+using Chronith.Infrastructure.Persistence;
 using FastEndpoints;
 using FastEndpoints.Security;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,13 @@ builder.Services
 builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
+
+// Run EF Core migrations on startup so the schema is always up to date
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ChronithDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.UseExceptionHandler(ExceptionHandlingMiddleware.Configure);
 app.UseAuthentication()
