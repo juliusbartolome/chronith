06b182e0e0cc1069cada0a8a89b59efba6c4dc48
Julius Bartolome <juliusbartolome@duck.com>
2026-03-22 00:16:47 +0800

    feat(infra): add migration AddApiKeyScopesDropRole

---
+++ src/Chronith.Infrastructure/Migrations/PostgreSQL/20260321161633_AddApiKeyScopesDropRole.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chronith.Infrastructure.Migrations.PostgreSQL
+{
+    /// <inheritdoc />
+    public partial class AddApiKeyScopesDropRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "chronith",
+                table: "tenant_api_keys");
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "scopes",
+                schema: "chronith",
+                table: "tenant_api_keys",
+                type: "text[]",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "scopes",
+                schema: "chronith",
+                table: "tenant_api_keys");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                schema: "chronith",
+                table: "tenant_api_keys",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
